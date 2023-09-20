3f67ca17eab1e0744db40e0cf5bb769441f661c6
manvimolpariya <manvimolpariya2002@gmail.com>
2023-09-20 16:01:34 +0530

    Random Joke Generator

---
+++ main.js
@@ -0,0 +1,42 @@
+var arr=[
+`A Mexican magician says he will disappear on the count of 3.<br> He says "Uno, dos..." poof.
+ He disappeared without a tres.`,`Why don't ants get sick?<br>Because they have little antybodies.`,
+`Did you hear about that actress that stabbed herself?Reece…
+Witherspoon?<br>
+No, it was with her knife!`,
+`A man rushed into a Doctor's surgery, shouting "help me please, I'm shrinking" The Doctor calmly said, "now settle down a bit"....."you'll just have to learn to be a little patient"`,
+`A human resource person was quizzing a new employee on the company’s safety manual. “And what steps do you take in case of a fire?” she asked. The new employee replied, “Quick ones.”`,
+`My boss calls me "The computer"
+
+Not because of my calculation skills but because I go to sleep when left unattended for 15 minutes.`,
+`Boss: Can you work this weekend?<br>
+Me: Yeah no worries but I'll probably be a bit late as public transport is slow on weekends.
+Boss: What time will you get here?
+Me: Monday.`,
+`So, today my boss asked me to pick up 6 cans of Sprite for a meeting.
+However, when I returned, I realized that I had picked 7 up instead.`,
+`Saying that Java is nice because it works on every OS is like saying that anal sex is 
+nice because it works on every gender.`,
+`Today I learned that changing random stuff until your program works is \"hacky\"
+ and a \"bad coding practice\" but if you
+ do it fast enough it's \"Machine Learning\" and pays 4x your current salary.`,
+ `"man1": "What's the difference between a gay guy and a refrigerator?",<br>
+ "man2": "A refrigerator doesn't fart when I pull my meat out of it."`,
+ `"friend1": "Why did the functional programmer get thrown out of school?",<br>
+ "friend2": "Because he refused to take classes."`,
+ `"friend1": "What's the difference between a school bus and a cactus?",<br>
+ "friend2": "A cactus keeps the little pricks on the outside."`,
+ `"// This line doesn't actually do anything, but the code stops working when I delete it."`,
+ `"Women are like KFC, once you're done with the breasts and thighs, 
+ you just have a greasy box to put your bone in."`,
+ `"boy1": "If I make you breakfast in bed, a simple thank you is all I need.",<br>
+ "boy2": "Not all this \"How the fuck did you get in my house?!\" nonsense."`,
+ `"Two reasons I don't give money to homeless people.<br>
+ 1) They are going to spend it all on drugs and alcohol<br>
+ 2) I am going to spend it all on drugs and alcohol."`,
+];
+        function func(){    
+        let index = Math.floor(Math.random()* (arr.length-1))
+        para1.innerHTML = arr[index]
+        }
+  
